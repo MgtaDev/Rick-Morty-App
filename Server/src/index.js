11f67d0b7c72c521ec688c4cd743654878f41ea3
Juan Pablo Mora <jmoracafe@gmail.com>
2023-05-17 20:45:42 -0600

    cambio del servidor a promises conectada a la api

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const data = require("./utils/data")
+const getCharById = require("./controllers/getCharById");
 
 http.createServer((req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -8,16 +8,9 @@ http.createServer((req, res) => {
     if(url.includes("rickandmorty/character")){
 
         let id = parseInt(url.substring(url.lastIndexOf("/")+1));
-        let response = data.find(character => character.id === id);
         
-        if(character){
-            res.writeHead(200, {"Contetn-type": "application/json"})
-            return res.end(JSON.stringify(response));
-        } else{
-            res.writeHead(404, {"Contetn-type": "application/json"})
-            return res.end(JSON.stringify({error: "Character not found"}))
-        }
-        
+        getCharById(res, id);
+
     } 
 
 }).listen(3001, "localhost")
